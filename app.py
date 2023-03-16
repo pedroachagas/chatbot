97c64258dcfbb93cf61c6c59d92951b6ffbbba89
Pedro Chagas <pchagas96@gmail.com>
2023-03-16 15:49:17 -0300

    created sidebar

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from langchain import OpenAI
 import os
 import streamlit as st
 
-def construct_index(directory_path):
+def construct_index(directory_path, api_key):
     # set maximum input size
     max_input_size = 4096
     # set number of output tokens
@@ -15,7 +15,7 @@ def construct_index(directory_path):
     chunk_size_limit = 600
 
     # define LLM (ChatGPT gpt-3.5-turbo)
-    llm_predictor = LLMPredictor(llm=OpenAI(temperature=0, model_name="gpt-3.5-turbo", max_tokens=num_outputs))
+    llm_predictor = LLMPredictor(llm=OpenAI(api_key=api_key, temperature=0, model_name="gpt-3.5-turbo", max_tokens=num_outputs))
     prompt_helper = PromptHelper(max_input_size, num_outputs, max_chunk_overlap, chunk_size_limit=chunk_size_limit)
  
     documents = SimpleDirectoryReader(directory_path).load_data()
@@ -40,21 +40,24 @@ def ask_me_anything(question, api_key):
     st.markdown(f"**ChatGPCela**: {response.response}")
 
 def main():
+    st.set_page_config(page_title="ChatGPCela", page_icon=":smiley:")
     st.title('ChatGPCela')
-    question = st.text_input('O que você gostaria de saber?')
     
-    # Add toggle for API key
-    use_user_api_key = st.checkbox('Usar sua chave de API do OpenAI')
-    if use_user_api_key:
-        api_key = st.text_input('Digite sua chave de API do OpenAI')
-    else:
-        api_key = st.secrets.openai_key
+    # Sidebar for index building button and API key toggle
+    with st.sidebar:
+        st.subheader("Configurações")
+        use_user_api_key = st.checkbox('Usar sua chave de API do OpenAI')
+        if use_user_api_key:
+            api_key = st.text_input('Digite sua chave de API do OpenAI')
+        else:
+            api_key = st.secrets.openai_key
 
-    # Add button to construct index
-    if st.button('Construir Índice'):
-        directory_path = 'textdata'
-        construct_index(directory_path)
-        st.markdown('Índice construído com sucesso!')
+        if st.button('Construir Índice'):
+            directory_path = 'textdata'
+            construct_index(directory_path, api_key)
+            st.markdown('Índice construído com sucesso!')
+
+    question = st.text_input('O que você gostaria de saber?')
 
     if st.button('Perguntar'):
         ask_me_anything(question, api_key)
